0e9e798761e25b4492b78cb5aaca49e5a856aa0f
Kayla Lane <kaylalane@ufl.edu>
2023-12-19 17:28:29 -0500

    CI: Navigate to client folder before running anything

---
+++ client/lib/dateSubtraction.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "vitest";
+
+test.todo("dateSubtraction", () => {
+    expect("testing").toBe("testing");
+});
---
+++ client/lib/dateSubtraction.ts
@@ -0,0 +1,4 @@
+//for calculating time since a comment was posted
+export default function dateSubtraction(date: string) {
+    return date;
+}
